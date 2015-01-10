4bd12cf4ff179c48dab68796625fa6f3d0195606
Vasu Srinivasan <vasu@skimo.tv>
2015-01-09 23:10:23 -0800

    added on complete event

--- m3u8dtest.js
+++ m3u8dtest.js
@@ -1,11 +1,11 @@
-var m3u8downloader = require('./lib/m3u8downloader.js');
+var m3u8downloader = require('./');
 var downloader = new m3u8downloader("http://www.nacentapps.com/m3u8/index.m3u8", "destination",
-function(data,err)
+function(err, data)
 {
     if(err)
         console.log(err);
     else
-        console.log(data);
+        console.dir(data)
 });
 
 downloader.on('start', function()
@@ -22,4 +22,10 @@ downloader.on('progress', function(d)
 downloader.on('downloaded', function(d)
 {
     console.log(d);
+});
+
+
+downloader.on('complete', function(d) 
+{
+    console.log('done');
 });
